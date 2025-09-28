f4473f20cbd8347da4fa586fbb50cadf84583f8e
PabloViniegra <pablovpmadrid@gmail.com>
2025-09-28 22:03:19 +0200

    add origin

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://how-are-u.netlify.app"
     ]
 
     MAX_FILE_SIZE: int = 10 * 1024 * 1024
